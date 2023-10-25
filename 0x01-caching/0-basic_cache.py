04052227f1629c0fefadb646ee165dd3f12d1e72
peter njihia <peternjihia2001@gmail.com>
2023-10-25 20:50:21 +0000

    fifo caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""  Basic dictionary """
+"""  basic dictionary """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system
-        This caching system doesn’t have limit """
+    """class that inherits from BaseCaching and is a caching system
+        this caching system doesn’t have limit """
     def put(self, key, item):
-        """ Assign to the dictionary """
+        """ assign to the dictionary """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Return the value linked """
+        """ return the value linked """
         if key is None or self.cache_data.get(key) is None:
             return None
         return self.cache_data.get(key)
